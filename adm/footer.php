822fa755e8e1753551a376c95755e9179f1443ba
Marat <1203Marat@gmail.com>
2020-12-18 23:37:48 +0600

    add docker-composer files

--- adm/footer.php
+++ adm/footer.php
@@ -10,13 +10,9 @@
     </div>
 
     <!-- jQuery -->
-    <script src="../assets/bootstrap/jquery.min.js"></script>
-	<!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> -->
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <!-- Bootstrap -->
-    <!-- <script src="../assets/bootstrap/js/bootstrap.min.js"></script> -->
-	
-	<script src="../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script> -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     <!-- bootstrap-progressbar -->
     <script src="js/bootstrap-progressbar.min.js"></script>
     <!-- Flot -->
